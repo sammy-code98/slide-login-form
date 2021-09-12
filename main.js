45a20f3c933dce8c3b1ce7bf071fe52487666964
sammy-code98 <sammysmart98@yahoo.com>
2021-09-12 02:48:18 +0100

    final update

--- main.js
+++ main.js
@@ -14,12 +14,15 @@ const form = document.getElementById("form");
 const fullName = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-
+const email2 = document.getElementById("email2");
+const pass2 = document.getElementById("pass2");
 
 // validator constants
 const nameVal = document.getElementById("nameVal");
 const emailVal = document.getElementById("emailVal");
 const passVal = document.getElementById("passVal");
+const emailVal2 = document.getElementById("emailVal2");
+const passVal2 = document.getElementById("passVal2");
 
 // validator function
 function validate() {
@@ -40,6 +43,23 @@ function validate() {
       passVal.style = "display:block";
     } else if (password.value != null) {
       passVal.style = "display:none";
+    }
+  }
+}
+
+function valid() {
+  if (email2.value && pass2.value) {
+    document.querySelector(".submiteBtn").type = "submit";
+  } else {
+    if (email2.value == "" || email2 == undefined) {
+      emailVal2.style = "display:block";
+    } else if (email2.value !== null) {
+      emailVal2.style = "display:none";
+    }
+    if (pass2.value == "" || pass2 == undefined) {
+      passVal2.style = "display:block";
+    } else if (passVal2.value != null) {
+      passVal2.style = "display:none";
     }
   }
 }
