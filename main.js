fdc9ebaabfa3d191d631e56696dd93aa5fe40716
sammy-code98 <sammysmart98@yahoo.com>
2021-09-12 02:09:37 +0100

    Feat:add javascript validation

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-const signUpButton =document.getElementById('signUp');
-const signInButton =document.getElementById('signIn');
-const container =document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 //ADD EVENT LISTENERS
-signUpButton.addEventListener('click',() => container.classList.add('right-panel-active'));
+signUpButton.addEventListener("click", () =>
+  container.classList.add("right-panel-active")
+);
 
+signInButton.addEventListener("click", () =>
+  container.classList.remove("right-panel-active")
+);
 
-signInButton.addEventListener('click',() => container.classList.remove('right-panel-active'));
+const form = document.getElementById("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+// validator constants
+const nameVal = document.getElementById("nameVal");
+const emailVal = document.getElementById("emailVal");
+const passVal = document.getElementById("passVal");
+
+// validator function
+function validate() {
+  if (fullName.value && email.value && password.value) {
+    document.querySelector(".submitBtn").type = "submit";
+  } else {
+    if (fullName.value == " " || fullName === undefined) {
+      nameVal.style = "display:block";
+    } else if (fullName.value !== null) {
+      nameVal.style = "display:none";
+    }
+    if (email.value == " " || email === undefined) {
+      emailVal.style = "display:block";
+    } else if (email.value !== null) {
+      emailVal.style = "display:none";
+    }
+    if (password.value == "" || password === undefined) {
+      passVal.style = "display:block";
+    } else if (password.value != null) {
+      passVal.style = "display:none";
+    }
+  }
+}
